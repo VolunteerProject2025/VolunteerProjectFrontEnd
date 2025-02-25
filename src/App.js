19f92b4cd38174e8d91f1e6a267dcc77d188ca6e
ttuan9909 <ttanhtuan2003@gmail.com>
2025-02-25 22:56:18 +0700

    frontend Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateProject from "./components/CreateProject";
+import ProjectsList from "./components/ProjectList";
+import ProjectDetails from "./components/ProjectDetails";
+import UpdateProject from "./components/UpdateProject";
+import DeleteProject from "./components/DeleteProject";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProjectsList />} />
+        <Route path="/create" element={<CreateProject />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+        <Route path="/projects/:id/edit" element={<UpdateProject />} />
+        <Route path="/delete" element={<DeleteProject />} />
+      </Routes>
+    </Router>
   );
 }
 
