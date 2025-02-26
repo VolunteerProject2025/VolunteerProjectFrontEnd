04bc62c747ff1efb09c54f900a34ee10b6fd7ecb
ttuan9909 <ttanhtuan2003@gmail.com>
2025-02-26 10:46:49 +0700

    update code

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -21,6 +21,7 @@ const CreateProject = () => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
+      
     });
 
     const data = await response.json();
