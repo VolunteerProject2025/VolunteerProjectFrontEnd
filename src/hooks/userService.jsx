543ac23c5c58b383abfe0a00c917205a80513350
129171 <135350963+129171@users.noreply.github.com>
2025-04-01 16:34:33 +0700

    update admin

--- src/hooks/userService.jsx
+++ src/hooks/userService.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:3000/users';
+const API_VOL = 'http://localhost:3000/volunteers';
+const API_ORG = 'http://localhost:3000/organizations';
 
 export const getAllUsers = () => {
     return axios.get(API_URL,{ withCredentials: true });
@@ -14,6 +16,10 @@ export const activeUser = (id) => {
     return axios.put(`${API_URL}/${id}/active`,{ withCredentials: true });
 };
 
-export const getAllOrg2 = () => {
-    return axios.get(API_URL,{ withCredentials: true });
+export const getDetailUserById = (id, role) => {
+    if (role === 'Volunteer') {
+        return axios.get(`${API_VOL}/user/${id}`,{ withCredentials: true });
+    } else {
+        return axios.get(`${API_ORG}/${id}/details`,{ withCredentials: true });
+    }
 };
--- src/pages/UsersManagement.jsx
+++ src/pages/UsersManagement.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { getAllUsers } from '../hooks/userService'
-import { Button, Table } from "react-bootstrap";
-import { inActiveUser, activeUser } from '../hooks/userService';
+import Modal from 'react-bootstrap/Modal';
+import { inActiveUser, activeUser, getDetailUserById } from '../hooks/userService';
 
 export function UsersManagement() {
     const [users, setUsers] = useState([]);
+    const [show, setShow] = useState(false);
+    const [user, setUser] = useState();
 
     useEffect(() => {
         loaduser();
@@ -39,7 +41,30 @@ export function UsersManagement() {
         } catch (error) {
             console.error("Lỗi: ", error);
         }
-    }; 
+    };
+
+    const handleGetUser = async (id, role) => {
+        try {
+            const user = await getDetailUserById(id, role);
+            console.log(user);
+            console.log(role);
+            
+            if (user) {
+                setUser(user.data.data ? user.data.data : user.data);
+            } else {
+                setUser({});
+            }
+
+        } catch (error) {
+            console.error("Lỗi: ", error);
+        }
+    }
+    
+    const handleShow = async (id, role) => {
+        await handleGetUser(id, role);
+
+        setShow(true);
+    };
 
     return (
         <>
@@ -64,11 +89,12 @@ export function UsersManagement() {
                                         {Array.isArray(users) && users.length > 0 ? (
                                             users.map((user, index) => (
                                                 <tr key={index}>
-                                                    <th scope="row" style={{cursor: 'pointer'}}
+                                                    <th scope="row" style={{ cursor: 'pointer' }}
                                                         onClick={() => {
                                                             if (window.getSelection().toString()) return;
-                                                            alert(user.fullName);}
-                                                    }>{user.fullName}</th>
+                                                            alert(user._id);
+                                                        }
+                                                        }>{user.fullName}</th>
                                                     <td>{user.email}</td>
                                                     <td>{user.role}</td>
                                                     <td>{user.status}</td>
@@ -115,7 +141,7 @@ export function UsersManagement() {
                                                         <div style={{ marginTop: -4 }}>
                                                             <i style={{ fontSize: 26, cursor: "pointer" }}
                                                                 className="bi bi-text-indent-right"
-                                                                // onClick={() => handleShow(org._id)}
+                                                                onClick={() => handleShow(user._id, user.role)}
                                                                 title='View Details'
                                                             />
                                                         </div>
@@ -134,6 +160,92 @@ export function UsersManagement() {
                     </div>
                 </div>
             </div>
+
+            <Modal
+                show={show}
+                onHide={() => setShow(false)}
+                size="xl"
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                        User Details
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{
+                    maxHeight: "100vh",
+                    overflowY: "auto",
+                    padding: "20px",
+                    borderRadius: "10px"
+                }}>
+                    {user ? (
+                        <div style={{ textAlign: 'center', padding: '20px' }}>
+                            <div style={{ marginBottom: '15px' }}>
+                                {user?.volunteer?.user?.img_profile || user?.organization?.org_profile ? (
+                                    <img
+                                        src={user?.volunteer?.user?.img_profile || user?.organization?.org_profile}
+                                        alt="UserPhoto"
+                                        style={{
+                                            maxWidth: '38%',
+                                            height: 'auto',
+                                            borderRadius: '10px',
+                                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
+                                        }}
+                                    />
+                                ) : (
+                                    <p>No image available</p>
+                                )}
+                            </div>
+
+                            <h2 style={{ fontSize: '2.8rem', fontWeight: 'bold', marginBottom: '10px', color: '#333' }}>
+                                {user?.volunteer?.fullName || user?.organization?.name || "Loading ..."}
+                            </h2>
+
+                            {user?.organization && <h5 style={{ fontSize: '2.8rem', fontWeight: 'bold', marginBottom: '10px', color: '#333' }}>
+                                {user?.organization?.description || "Loading ..."}
+                            </h5>}
+
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                background: '#f8f9fa',
+                                padding: '15px',
+                                borderRadius: '8px',
+                                maxWidth: '800px',
+                                margin: 'auto',
+                                fontSize: '18px',
+                                boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)'
+                            }}>
+                                {[
+                                    user?.organization && { label: "Representative", value: user?.organization?.user.fullName },
+                                    { label: "Email", value: user?.volunteer?.email || user?.organization?.user.email },
+                                    user?.volunteer && { label: "Skills", value: user.volunteer.skills.name },
+                                    user?.volunteer && { label: "DateOfBirth", value: user?.volunteer?.dateOfBirth?.split("T")[0] },
+                                    user?.volunteer && { label: "Gender", value: user?.volunteer?.gender },
+                                    { label: "Location", value: user?.volunteer?.location || user?.organization?.address },
+                                    { label: "Phone", value: user?.volunteer?.phone || user?.organization?.phone }
+                                ].filter(Boolean)
+                                .map((item, index) => (
+                                    <div key={index} style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        width: '100%',
+                                        padding: '5px 0',
+                                        borderBottom: index !== 5 ? '1px solid #ddd' : 'none'
+                                    }}>
+                                        <strong style={{ color: '#555' }}>{item.label}:</strong>
+                                        <span style={{ maxWidth: 500 }}>{item.value || "Loading ..."}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ) : (
+                        <p style={{ textAlign: 'center', padding: '20px' }}>Loading data...</p>
+                    )}
+                </Modal.Body>
+            </Modal>
         </>
     );
 }
